6b50a0bf6f8b32eb057752a8559ba0048e3590fc
abiyazhini <vaanchinathan77@gmail.com>
2023-02-06 08:22:02 +0530

    abi task 06-02-2023

---
+++ src/org/emp/Employee2.java
@@ -0,0 +1,38 @@
+package org.emp;
+
+public class Employee2 {
+	int a=10,b=11,c;
+	public void empId() {
+	System.out.println("Employee ID: 1000");
+	}
+	public void empName() {
+		System.out.println("Employee Name: Vanchi");
+	}
+	public void empDob() {
+		System.out.println("Employee DOB: 02.05.1987");
+	}
+	
+	public void empPhone() {
+		System.out.println("Employee Phone: 9500705770");
+	}
+	public void empEmail() {
+		System.out.println("Employee : abc@gmail.com");
+		c=a+b;
+	}
+	
+		
+		
+	
+	
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.empId();
+		emp.empName();
+		emp.empDob();
+		emp.empPhone();
+		emp.empEmail();
+		//emp.empAddress();
+
+	}
+
+}
